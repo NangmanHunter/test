c09c13bd2a0429b45fca1452cb705081dfe2d8b6
djwlfdls1 <djwlfdls1@gmail.com>
2025-04-24 22:30:31 +0900

    test

--- res/test/test.py
+++ res/test/test.py
@@ -1,3 +1,16 @@
-﻿a = "a"
-if a == "a":
-    a = b
+import subprocess
+
+# 실행할 .py 파일 목록
+scripts = ["test01.py", "test02.py", "test03.py"]
+
+for script in scripts:
+    print(f"Running {script}...")
+    subprocess.run(["python", script])
+
+
+
+
+'''
+Run
+- py test.py
+'''
