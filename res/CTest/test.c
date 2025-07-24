a087bc09dbe0ec8f4fb10d423c34107d504707f9
djwlfdls1 <djwlfdls1@gmail.com>
2025-07-24 16:05:15 +0900

    test

---
+++ res/CTest/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("%d", 1==1);  // 출력: 1
+    printf("%d", 1==2);  // 출력: 0
+    return 0;
+}
---
+++ res/JavaTest/test/src/Test.java
@@ -0,0 +1,8 @@
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.err.println(1==1);
+        System.err.println(1==2);
+    }
+}
